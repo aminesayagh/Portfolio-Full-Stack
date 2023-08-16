ba98682d0ee6eb3485102508e1d6a639ad66cb10
aminesayagh <aminesayagh1997@gmail.com>
2023-08-16 06:45:43 +0000

    call to action 1

--- components/pages/sections/3_1-action/Action.tsx
+++ components/pages/sections/3_1-action/Action.tsx
@@ -8,8 +8,8 @@ const Action = () => {
     const { t } = useTranslation()
     return (
         <>
-            <div className={twMerge('flex flex-row justify-between items-end', 'py-12')}>
-                <div className='w-auto flex flex-row items-center justify-start'>
+            <div className={twMerge('flex flex-col sm:flex-row justify-between items-start sm:items-end', 'gap-8 sm:gap-0', 'py-12')}>
+                <div className={twMerge('w-auto flex flex-row items-center justify-start', 'order-2 md:order-1')}>
                     <Button size='sm' className='border rounded-full py-4 px-6' degree='2' >
                         {t('motivation.action')}
                     </Button>
@@ -17,7 +17,7 @@ const Action = () => {
                         <Icon name='IconArrowUpRight' size='22' className='stroke-white-100 stroke-[2px]' />
                     </Button>
                 </div>
-                <div className={twMerge('flex flex-col gap-4', 'w-5/12')} >
+                <div className={twMerge('flex flex-col gap-4', 'w-full xxs:w-11/12 xs:w-10/12 sm:w-7/12 lg:w-1/2 xl:w-5/12', 'order-1 md:order-2')} >
                     <Text p degree="3" weight='semibold' size='md' >
                         {t('motivation.content.1')}    
                     </Text> 
