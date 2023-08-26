2fe3dbe66d2f820280636aade40509dbcce642dc
aminesayagh <aminesayagh1997@gmail.com>
2023-08-26 17:27:17 +0000

    update

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: NavbarType = ({ children, size, className, inTopOfScroll, ...props
 
     useEffect(() => {
         scrollbar && scrollbar.addListener((status) => {
-            if (status.offset.y < 600) {
+            if (status.offset.y < 200) {
                 !!active && setActive(false);
             } else {
                 !active && setActive(true);
