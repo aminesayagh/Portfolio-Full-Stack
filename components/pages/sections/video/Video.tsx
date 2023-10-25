17ef3227f74059f0cd4930bb9e308d2e54f7c4a0
aminesayagh <aminesayagh1997@gmail.com>
2023-10-25 07:26:18 +0100

    update component video

--- components/pages/sections/video/Video.tsx
+++ components/pages/sections/video/Video.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useContext } from 'react'
+import { useRef, useState, useContext, useEffect, useCallback } from 'react'
 import { gsap } from '@/utils/gsap';
 
 import { twMerge } from 'tailwind-merge';
@@ -6,84 +6,102 @@ import { twMerge } from 'tailwind-merge';
 import { rounded } from '@/components/style';
 import { CursorContent, LoadingContext } from '@/components/ui';
 import useGsap from '@/hook/useGsap';
+import { ScrollProvider } from '@/context/AnimationConf';
 
-
+const FRAME_COUNT = 164;
 const Video = () => {
     let ref = useRef<HTMLCanvasElement>(null);
     let refContainer = useRef<HTMLDivElement>(null);
-    const [images, setImages] = useState<Array<HTMLImageElement>>([]);
+    const imagesRef = useRef([] as Array<HTMLImageElement>);
     const { addLoadingComponent, removeLoadingComponent } = useContext(LoadingContext);
-    
-    useGsap(() => {
+    useEffect(() => {
+        let isInLoad = false;
         addLoadingComponent('video');
-        let canvas = ref.current;
-        if (!canvas) return;
-        let context = canvas.getContext('2d');
 
-        canvas.width = 1488;
-        canvas.height = 1100;
 
-        const frameCount = 164;
         const currentFrame = (index: number) => `/framer-image/ezgif-frame-${index.toString().padStart(3, '0')}.webp`;
-        for (let i = 1; i <= frameCount; i++) {
+        Promise.all<HTMLImageElement>(Array(FRAME_COUNT).fill(0).map((_, index) => {
             const img = new Image();
-            img.src = currentFrame(i);
+            img.src = currentFrame(index + 1);
             img.width = 1488;
             img.height = 1100;
-            img.alt = 'video' + '_' + i.toString().padStart(3, '0');
+            img.alt = 'video' + '_' + index.toString().padStart(3, '0');
             img.style.objectFit = 'cover';
-            img.style.objectPosition = 'center'; 
-
-            images.push(img);
-        }
-
-        const hands = { frame: 0 };
-
-        gsap.to(hands, {
-            frame: frameCount - 1,
-            snap: 'frame',
-            ease: 'none',
-            scrollTrigger: {
-                scrub: 0.5,
-                trigger: canvas,
-            },
-            onUpdate: render
+            img.style.objectPosition = 'center';
+            return new Promise((resolve, reject) => {
+                img.onload = () => {
+                    resolve(img);
+                }
+            })
+        })).then((images) => {
+            if (isInLoad) return;
+            console.log(images);
+            imagesRef.current = images;
+            removeLoadingComponent('video');
         })
-        
-        images[0].onload = render;
-        setImages(() => images);
-
-        function render() {
-            if(!context) return;
-            if(!ref.current) return;
-            context?.clearRect(0, 0, ref.current.width, ref.current.height);
-            context?.drawImage(images[hands.frame], 0, 0);
-        }
-        removeLoadingComponent('video');
-    }, ref);
-    useGsap(() => {
-        gsap.fromTo('.video_gsap', {
-            opacity: 0,
-        }, {
-            opacity: 1,
-            ease: 'power4',
-            duration: 0.5,
-            scrollTrigger: {
-                trigger: '.video_gsap',
+        return () => {
+            isInLoad = true;
+            if (imagesRef.current) {
+                imagesRef.current.forEach((image) => {
+                    image.src = '';
+                })
             }
-        })  
-    }, refContainer)
+        }
+    }, [removeLoadingComponent, addLoadingComponent])
+    const { scrollbar } = useContext(ScrollProvider);
 
+    useEffect(() => {
+        const ctx = gsap.context(() => {
+            if(!imagesRef.current.length) return;
+            if(!scrollbar) return;
+            const hands = { frame: 0 };
+
+            let canvas = ref.current;
+            if (!canvas) return;
+            let context = canvas.getContext('2d');
+
+            canvas.width = 1488;
+            canvas.height = 1100;
+
+            gsap.to(hands, {
+                frame: FRAME_COUNT - 1,
+                snap: 'frame',
+                ease: 'none',
+                scrollTrigger: {
+                    scrub: 0.5,
+                    trigger: canvas,
+                },
+                onUpdate: render
+            });
+
+            imagesRef.current[0].onload = render;
+
+            function render() {
+                if(!imagesRef.current.length) return;
+                console.log('render', imagesRef.current)
+                if (!context) return;
+                if (!ref.current) return;
+                context?.clearRect(0, 0, ref.current.width, ref.current.height);
+                context?.drawImage(imagesRef.current[hands.frame], 0, 0);
+            }
+        }, refContainer);
+        return () => {
+            ctx.revert();
+        }
+    }, [imagesRef.current.length, refContainer, scrollbar])
+    // const render = useCallback(() => {
+
+    // }, [])
     return (
         <>
             <div data-scroll ref={refContainer} className={twMerge('block relative w-full h-fit rounded-3xl video_gsap overflow-hidden will-change-transform-animation', rounded({ size: 'xl' }))}>
-                <CursorContent name='CursorScrollVideo' component='CursorScroll' props={{ title: 'scroll' }}> 
+                <CursorContent name='CursorScrollVideo' component='CursorScroll' props={{ title: 'scroll' }}>
                     <canvas data-scroll ref={ref} className={twMerge(
-                        'h-full w-full will-change-transform-animation', 
+                        'h-full w-full will-change-transform-animation',
                         'h-[70vh] xxs:h-screen sm:h-[96vh] lg:h-screen',
-                        'max-h-[720px] xxs:max-h-[900px] sm:max-h-[800px] lg:max-h-[830px] xl:max-h-[900px]', 
+                        'max-h-[720px] xxs:max-h-[900px] sm:max-h-[800px] lg:max-h-[830px] xl:max-h-[900px]',
                         rounded({ size: 'xl' })
-                    )} style={{ width: "100%", objectFit: 'cover'  }} />
+                    )} style={{ width: "100%", objectFit: 'cover' }} />
                 </CursorContent>
             </div>
         </>
