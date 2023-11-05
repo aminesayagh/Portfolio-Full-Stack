22732894bca67d32865c7912cea67e3e35030665
aminesayagh <aminesayagh1997@gmail.com>
2023-11-05 14:06:49 +0100

    correction noise effect

--- components/ui/noise/Noise.tsx
+++ components/ui/noise/Noise.tsx
@@ -7,7 +7,7 @@ const Noise = ({ position = 'fixed', className = 'opacity-90' }: { position?: 'f
         Style['bg'],
         className || 'opacity-90',
         position == 'fixed'
-            ? 'fixed -top-1/2 -left-1/2 -bottom-1/2 -right-1/2 w-[200%] h-[200vh] visible z-bg'
+            ? 'fixed -top-1/2 -left-1/2 -bottom-1/2 -right-1/2 w-[200vw] h-[200vh] visible z-bg'
             : 'absolute overflow-hidden -left-1/2 -top-1/2 w-[200%] h-[200%] overflow-none z-50'
         )}
     >
