86c66987009ea77a6a6ca5da596eae5da99c583a
aminesayagh <aminesayagh1997@gmail.com>
2024-04-16 15:39:40 +0000

    Update download filename to references.pdf in references.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import useSafePush from "@/hook/SafePush";
+import { useEffect}  from 'react';
+import useSafePush from '@/hook/SafePush';
 
 function References() {
     const push = useSafePush()
     useEffect(() => {
         // start a donwload file on the client side when the page is loaded
         const downloadFile = async () => {
-            const response = await fetch('/references.pdf');
+            const response = await fetch('/resume.pdf');
             const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement('a');
             a.href = url;
-            a.download = 'references.pdf';
+            a.download = 'resume.pdf';
             a.click();
             a.remove();
         }
