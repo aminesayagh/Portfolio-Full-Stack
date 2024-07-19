bb7e6bf091fd2c60ef271d9d2292ecb9ec33d79b
aminesayagh <aminesayagh1997@gmail.com>
2024-07-19 14:05:30 +0000

    correct robots.ts api

--- pages/api/robots.ts
+++ pages/api/robots.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (_: NextApiRequest, res: NextApiResponse) => {
   res.setHeader("Content-Type", "text/plain");
   res.write(`User-agent: *
         Disallow: /api/
